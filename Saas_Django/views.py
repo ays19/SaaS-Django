7b3b0062781f652067d6a944814bc3f4cf71f8dc
ays19 <sahsanyasir@gmail.com>
2026-07-15 21:38:57 +0600

    feat: add home.html and update home_page_view to render HTML document with page_title context

--- Saas_Django/views.py
+++ Saas_Django/views.py
@@ -1,5 +1,52 @@
+
+import pathlib
 from django.http import HttpResponse
 
-def home_page_view(request, *args, **kwargs):
+this_dir = pathlib.Path(__file__).parent
 
-    return HttpResponse("<h1>Welcome to the Home Page</h1>")
+def home_page_view(request, *args, **kwargs):
+    my_title = "My Page"
+    my_context = {
+        "page_title": my_title
+    }
+    html_ = """
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>{page_title} anything</h1>
+</body>
+</html>
+""".format(**my_context) # page_title = my_title
+    #html_file_path = this_dir / "home.html"
+    #html_ = html_file_path.read_text()
+
+    return HttpResponse(html_)
+
+
+def home_page_view(request, *args, **kwargs):
+    my_title = "My Page"
+    my_context = {
+        "page_title": my_title
+    }
+    html_ = """
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>{page_title} anything</h1>
+</body>
+</html>
+""".format(**my_context) # page_title = my_title
+    #html_file_path = this_dir / "home.html"
+    #html_ = html_file_path.read_text()
+
+    return HttpResponse(html_)
