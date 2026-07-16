bc7928bb2c119cd65fbf1484a1e097a66501dce8
ays19 <sahsanyasir@gmail.com>
2026-07-16 15:22:35 +0600

    feat: implement PageVisit logging in home view and display visit counts

--- Saas_Django/views.py
+++ Saas_Django/views.py
@@ -1,16 +1,31 @@
-
 import pathlib
 from django.shortcuts import render
 from django.http import HttpResponse
+from visits.models import PageVisit
 
 this_dir = pathlib.Path(__file__).resolve().parent
 
 def home_page_view(request, *args, **kwargs):
+    qs = PageVisit.objects.all() #qs = queryset
+    page_qs = PageVisit.objects.filter(path=request.path)
+
     my_title = "My Page"
+    PageVisit.objects.create(path=request.path)
+    my_context = {
+        "page_title": my_title,
+        "page_visit_count": page_qs.count(),
+        "percent": (page_qs.count() / qs.count()) * 100 if qs.count() > 0 else 0,
+        "total_visit_count": qs.count()
+    }
+    html_template = "home.html"
+    return render(request, html_template, my_context)
+
+def about_page_view(request, *args, **kwargs):
+    my_title = "About Page"
     my_context = {
         "page_title": my_title
     }
-    html_template = "home.html"
+    html_template = "about.html"
     return render(request, html_template, my_context)
 
 
