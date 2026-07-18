07174b3200c088dc905b224e1921e613842d8f0c
ays19 <sahsanyasir@gmail.com>
2026-07-18 12:08:50 +0600

    Add commando app with hello_world management command

---
+++ commando/management/commands/hello_world.py
@@ -0,0 +1,8 @@
+from typing import Any
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+
+    def handle(self, *arg: Any, **options: Any):
+        print("Hello, World!")
