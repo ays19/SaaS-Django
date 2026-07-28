4efafcdb954e49cdae5843b234124cd6745cd97c
ays19 <sahsanyasir@gmail.com>
2026-07-28 22:35:28 +0600

    Add password-protected and login-required views with templates

--- Saas_Django/urls.py
+++ Saas_Django/urls.py
@@ -17,13 +17,20 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from auth import views as auth_views
-from .views import home_view, about_view
+from .views import (
+    home_view, 
+    about_view, 
+    pw_protected_view,
+    user_only_view
+)
 
 urlpatterns = [
-    path('', home_view, name='home'),
+    path('', home_view, name='home'), #index page -> root page
     path('login/', auth_views.login_view, name='login'),
     path('register/', auth_views.register_view, name='register'),
     path('about/', about_view, name='about'),
     path('accounts/', include('allauth.urls')),
+    path('protected/user_only/', user_only_view),
+    path('protected/', pw_protected_view),
     path('admin/', admin.site.urls),
 ]
