2f5980af0ad974ad74aaae6197f262778685f358
ays19 <sahsanyasir@gmail.com>
2026-07-29 08:28:29 +0600

    Add staff_only_view and protected staff route

--- Saas_Django/urls.py
+++ Saas_Django/urls.py
@@ -21,7 +21,8 @@ from .views import (
     home_view, 
     about_view, 
     pw_protected_view,
-    user_only_view
+    user_only_view,
+    staff_only_view
 )
 
 urlpatterns = [
@@ -31,6 +32,7 @@ urlpatterns = [
     path('about/', about_view, name='about'),
     path('accounts/', include('allauth.urls')),
     path('protected/user_only/', user_only_view),
+    path('protected/staff_only/', staff_only_view),
     path('protected/', pw_protected_view),
     path('admin/', admin.site.urls),
 ]
--- Saas_Django/views.py
+++ Saas_Django/views.py
@@ -1,8 +1,12 @@
 import pathlib
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.contrib.admin.views.decorators import staff_member_required
+from django.conf import settings
 from django.http import HttpResponse
 from visits.models import PageVisit
+
+LOGIN_URL = settings.LOGIN_URL
 
 this_dir = pathlib.Path(__file__).resolve().parent
 
@@ -71,4 +75,9 @@ def pw_protected_view(request, *args, **kwargs):
 
 @login_required
 def user_only_view(request, *args, **kwargs):
+    # print(request.user.is_staff)
+    return render(request, "protected/user_only.html", {})
+
+@staff_member_required(login_url=LOGIN_URL)
+def staff_only_view(request, *args, **kwargs):
     return render(request, "protected/user_only.html", {})
