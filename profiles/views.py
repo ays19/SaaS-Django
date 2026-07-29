07137dd6f1272f573f42ecd6eb77501ca0b0b13d
ays19 <sahsanyasir@gmail.com>
2026-07-29 18:21:48 +0600

    Refactor profile view to profile_detail_view and add detail template

--- profiles/views.py
+++ profiles/views.py
@@ -14,15 +14,13 @@ def profile_list_view(request):
 
 # Create your views here.
 @login_required
-def profile_view(request, username=None, *args, **kwargs):
+def profile_detail_view(request, username=None, *args, **kwargs):
     user = request.user
-    print('user.has_perm("auth.view_user")', user.has_perm("auth.view_user"))
-    # profile_user_obj = User.objects.get(username=username)
-     
     profile_user_obj = get_object_or_404(User, username=username)
     is_me = profile_user_obj == user
-    if is_me:
-        if user.has_perm("visits.view_pagevisit"):
-            pass
-            # qs = PageVisit.objects.all()
-    return HttpResponse(f"Hello there {username} - {profile_user_obj.id} - {user.id} - {is_me}")
+    context = {
+           "object": profile_user_obj,
+           "instance": profile_user_obj,
+           "owner": is_me,
+       }
+    return render(request, "profiles/detail.html", context)
