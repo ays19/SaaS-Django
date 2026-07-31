37ce7ff3e714668fc59a9ece66a1ad0d461e21a8
ays19 <sahsanyasir@gmail.com>
2026-07-31 16:08:45 +0600

    feat(billing): integrate Stripe SDK and add billing helper module
    - Add stripe dependency to requirements.txt.
    
    - Create helpers/billing.py module initialized with STRIPE_SECRET_KEY from environment configurations.

---
+++ helpers/billing.py
@@ -0,0 +1,13 @@
+import stripe
+from decouple import config
+
+DJANGO_DEBUG=config("DJANGO_DEBUG", default=False, cast=bool)
+STRIPE_SECRET_KEY = config("STRIPE_SECRET_KEY", default="", cast=str)
+
+stripe.api_key = STRIPE_SECRET_KEY
+
+# def create_customer():
+#     stripe.Customer.create(
+#     name="Jenny Rosen",
+#     email="jennyrosen@example.com",
+# )
