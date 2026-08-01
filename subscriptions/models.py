0e10ac51a80fba94704f66cecb39711846a6e29a
ays19 <sahsanyasir@gmail.com>
2026-08-01 11:31:13 +0600

    feat(subscriptions): add SubscriptionPrice model and integrate Stripe price creation
    - Add SubscriptionPrice model with interval choices (Monthly/Yearly) and decimal price formatting
    
    - Implement create_price helper function in helpers/billing.py via Stripe SDK
    
    - Auto-create Stripe Price object on SubscriptionPrice.save() when product_stripe_id is set
    
    - Add migration 0006_subscriptionprice.py

--- subscriptions/models.py
+++ subscriptions/models.py
@@ -35,9 +35,62 @@ class Subscription(models.Model):
 
     def save(self, *args, **kwargs):
             if not self.stripe_id:
-                stripe_id = helpers.billing.create_product(name=self.name, metadata={"subscription_plan_id": self.id}, raw=False)
+                stripe_id = helpers.billing.create_product(name=self.name, metadata={
+                    "subscription_plan_id": self.id
+                }, 
+                raw=False)
                 self.stripe_id = stripe_id                     
             super().save(*args, **kwargs)
+
+
+# Create your models here.
+class SubscriptionPrice(models.Model):
+    """
+    Subscription Price = Stripe Price
+    """
+    class IntervalChoices(models.TextChoices):
+        MONTHLY = "month", "Monthly"
+        YEARLY = "year", "Yearly"
+
+    subscription = models.ForeignKey(Subscription, on_delete=models.SET_NULL, null=True)
+    stripe_id = models.CharField(max_length=120, null=True, blank=True)
+    interval = models.CharField(max_length=120,
+                                default=IntervalChoices.MONTHLY,
+                                choices=IntervalChoices.choices)
+    price = models.DecimalField(max_digits=10, decimal_places=2, default=99.99)
+
+    @property
+    def stripe_currency(self):
+        return "usd"
+
+    @property
+    def stripe_price(self):
+        """
+        remove decimal places
+        """
+        return self.price * 100
+
+    @property
+    def product_stripe_id(self):
+        if not self.subscription:
+            return None
+        return self.subscription.stripe_id
+
+    def save(self, *args, **kwargs):
+        if (not self.stripe_id and
+            self.product_stripe_id is not None):
+            stripe_id = helpers.billing.create_price(
+                currency= self.stripe_currency,
+                unit_amount=self.stripe_price,
+                interval=self.interval,
+                product=self.product_stripe_id,
+                metadata={
+                    "subscription_plan_price_id": self.id
+                },
+                raw=False
+            )
+            self.stripe_id = stripe_id
+        super().save(*args, **kwargs)
 
 
 class UserSubscription(models.Model):
