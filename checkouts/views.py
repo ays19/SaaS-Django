2a21cd17a1f391e14d5536e7646927c50e0ee3ca
ays19 <sahsanyasir@gmail.com>
2026-08-04 20:39:25 +0600

    feat(checkouts): integrate Stripe Checkout session initiation and completion views
    - Implement start_checkout_session, get_checkout_session, and get_subscription helpers in helpers/billing.py
    - Update checkout_redirect_view to initiate Stripe Checkout session and redirect user
    - Update checkout_finalize_view to retrieve session & subscription details and render success template
    - Add templates/checkout/success.html template for checkout completion display
    - Add BASE_URL setting in Saas_Django/settings.py for checkout redirect callback URLs

--- checkouts/views.py
+++ checkouts/views.py
@@ -1,7 +1,12 @@
+import helpers.billing
 from django.shortcuts import render, redirect
+from django.urls import reverse
 from django.contrib.auth.decorators import login_required
+from django.conf import settings
+
 from subscriptions.models import SubscriptionPrice
 
+BASE_URL = settings.BASE_URL
 # Create your views here.
 def product_price_redirect_view(request, price_id=None, *args, **kwargs):
     request.session['checkout_subscription_price_id'] = price_id
@@ -17,10 +22,36 @@ def checkout_redirect_view(request):
     if checkout_subscription_price_id is None or obj is None:
         return redirect("pricing")
     customer_stripe_id = request.user.customer.stripe_id
-    print(customer_stripe_id)
-    return redirect("/checkout/abac")
+    success_url_path = reverse("stripe-checkout-end")
+    pricing_url_path = reverse("pricing")
+    success_url = f"{BASE_URL}{success_url_path}"
+    cancel_url = f"{BASE_URL}{pricing_url_path}"
+    price_stripe_id = obj.stripe_id
+
+    url = helpers.billing.start_checkout_session(
+        customer_stripe_id,
+        success_url=success_url,
+        cancel_url=cancel_url,
+        price_stripe_id=price_stripe_id,
+        raw=False
+        
+    )
+    return redirect(url)
 
 
-
-def checkout_finalize_view(request):
-    return
+def checkout_finalize_view(request): #here all things coming from stripe
+    session_id = request.GET.get('session_id')
+    checkout_r = helpers.billing.get_checkout_session(session_id, raw=True)
+    customer_id = checkout_r.customer
+    sub_stripe_id = checkout_r.subscription
+    sub_r = helpers.billing.get_subscription(sub_stripe_id, raw=True)
+    sub_plan = sub_r.plan
+    sub_plan_price_stripe_id = sub_plan.id
+    price_qs = SubscriptionPrice.objects.filter(stripe_id=sub_plan_price_stripe_id)
+    print(price_qs)
+    # print(sub_r)
+    context = {
+        "subscription": sub_r,
+        "checkout": checkout_r
+    }
+    return render(request, "checkout/success.html")
